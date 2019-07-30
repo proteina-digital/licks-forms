b296e89d86bf89a80b0184d7c58f2bca81fd857c
willa <willa@Joaos-iMac.local>
2019-07-30 11:21:37 -0300

    work-with-us form teste

--- master.js
+++ master.js
@@ -1,3 +1,59 @@
+var send_form_file = function (form, event, url) {
+  event.preventDefault();
+  var btn = form.find("input[type='submit']");
+  var form_dados;
+
+
+  $('#fileUpload').change(function (event) {
+      form_dados = new FormData(form);
+      form_dados.append('fileUpload', event.target.files[0]); // para apenas 1 arquivo
+      //var name = event.target.files[0].content.name; // para capturar o nome do arquivo com sua extenção
+  });
+
+  $.ajax({
+      url: url, // Url do lado server que vai receber o arquivo
+      data: form_dados,
+      processData: false,
+      contentType: false,
+      type: 'POST',
+      beforeSend: function() {
+          btn.val( "SENDING..." );
+      },
+      success: function (data, textStatus, xhr) {
+          console.log("resposta: "+data);
+          console.log("status: "+data.status);
+          console.log("xhr: "+xhr.status);
+
+          if ( typeof form.attr('redirect') !== 'undefined'  ) {
+            window.location = 'http://lickslegal.com/'+form.attr('redirect');
+          }else{
+            form.parent("div").find('.w-form-done').slideDown();
+            form.parent("div").find('.w-form-fail').hide();
+            form.hide();
+          }
+      },
+      error: function(xhr,er) {
+
+          console.log('Error ' + xhr.status + ' - ' + xhr.statusText + ' - Tipo de erro: ' + er);
+
+          var erros = [];
+
+          if (xhr.status === 422) {
+
+            erros.push("Seu formulário não foi enviado. Verifique os dados e tente novamente.");
+           
+          } else {
+            erros.push("Erro no Servidor");
+          }
+          form.parent("div").find('.w-form-fail').html("<p>"+erros[0]+"</p>");
+          form.parent("div").find('.w-form-fail').slideDown();
+          btn.val("Subscribe");
+        }
+  });
+
+}
+
+
 var send_form = function(form, event, url){
     event.preventDefault();
     var serializeDados = form.serialize();
@@ -58,14 +114,15 @@ Webflow.push(function(){
   }
 
   // news form
-    $('form[name="wf-form-newsletter"]').submit(function(event) {
-        var form = $(this);
-        send_form(form, event, 'http://static.lickslegal.com/mailchimp/index.php');
-    });
+  $('form[name="wf-form-newsletter"]').submit(function(event) {
+      var form = $(this);
+      send_form(form, event, 'http://static.lickslegal.com/mailchimp/index.php');
+  });
 
   // work-with-us form
   $('form[name="wf-form-work-with-us"]').submit(function(event) {
-      var form = $(this);
-      send_form(form, event, 'http://localhost/lickslegal/work-with-us/index.php');
-  })
+    var form = $(this);
+    send_form_file(form, event, 'http://localhost/lickslegal/work-with-us/index.php');
+  });
+
 });
