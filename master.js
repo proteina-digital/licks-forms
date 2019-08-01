6e300ded6b3b00a44144345c94055f4944f8d952
willa <willa@Joaos-iMac.local>
2019-08-01 16:12:45 -0300

    msg de erro

--- master.js
+++ master.js
@@ -61,20 +61,20 @@ Webflow.push(function() {
                 method: 'post',
                 body: formData
             }).then(function(response) {
-                form = $('form[name="wf-form-work-with-us"]');
 
                 if (response.status >= 200) {
-                    return response.text();
-                    form.parent("div").find('.w-form-done').slideDown();
-                    form.parent("div").find('.w-form-fail').hide();
-                    form.hide();
+                  return response.text();
+                  work_form.style.display('none');
+                  document.querySelector('.w-form-done').style.display('block');
+                  document.querySelector('.w-form-fail').style.display('none');
+
                 } else {
-                  form.parent("div").find('.w-form-fail').html('Something went wrong...');
-                  form.parent("div").find('.w-form-fail').slideDown();
+                  document.querySelector('.w-form-fail').textContent('Something went wrong...');
+                  document.querySelector('.w-form-fail').style.display('block');
                 }
                 //throw new Error(response.statusText);
             }).then(function(response) {
-                console.log(response);
+                console.log(response);       
             }).catch(console.error);
         });
     }
